6f34e7581564732d7bfa3b0ade58272d8c3ed333
saloed <napster_1997@mail.ru>
2018-10-20 02:30:08 +0300

    tmp

--- Task3_3.hs
+++ Task3_3.hs
@@ -4,3 +4,19 @@ newtype PSet a = PSet{ contains :: (a -> Bool) }
 
 -- Реализуйте классы Monoid и Functor
 -- Объясните в комментариях, почему они реализованы именно так
+
+
+-- Тип PSet является контравариантным для a. 
+-- Для него нельзя определить Functor, т.к. он ковариантный
+-- Можно определить только контрвариантый функтор
+class Contravariant f where
+  contramap :: (b -> a) -> f a -> f b
+-- Определения взято из пакета Data.Functor.Contravariant
+
+-- Так как для одного типа несколько определений завести нельзя
+-- ниже объявлены несколько типов PSet
+
+newtype PSet a = PSet{ contains :: (a -> Bool) }
+newtype PSet a = PSet{ contains :: (a -> Bool) }
+newtype PSet a = PSet{ contains :: (a -> Bool) }
+newtype PSet a = PSet{ contains :: (a -> Bool) }
