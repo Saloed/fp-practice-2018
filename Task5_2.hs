4ded898341141d2008747df10a5caceb8d1cd4af
saloed <napster_1997@mail.ru>
2018-11-03 01:52:04 +0300

    another sin

--- Task5_2.hs
+++ Task5_2.hs
@@ -17,6 +17,10 @@ generate :: a -> (a -> a) -> Stream a
 generate x f =
     Cons x $ generate (f x) f
 
+generateWithIndex x i f =
+  let value = f x i in Cons value $ generateWithIndex value (i + 1) f
+
+
 instance Functor Stream where
     fmap f (Cons h t) = Cons (f h) (fmap f t)
 
@@ -31,8 +35,32 @@ instance Monad Stream where
     return x = srepeat x
     ss >>= f = sflatten (f <$> ss)
 
+sinFun x n = let p = 2 * n + 1 in ((-1) ** n) * (x ** p) / (product [1 .. p])
+
+halfPi = pi / 2
+
+eFun n = 1 / (product [1 .. n])
+
+moduloHelper :: Double -> Double -> Double
+moduloHelper y m | y > 0 && m >= y  = 0
+                 | y <= 0 && m <= y = 0
+                 | y > 0 && m < 0   = if (y + m == y) then 0 else y + m
+                 | y <= 0 && m > 0  = if (y + m == y) then 0 else y + m
+                 | otherwise        = m
+
+modulo :: Double -> Double -> Double
+modulo x 0 = x
+modulo x y =
+  let m = x - y * (fromIntegral . floor) (x / y) in moduloHelper y m
+
+sinBoundary :: Double -> Double
+sinBoundary x = (modulo (x + halfPi) (2 * pi)) - halfPi
+
+series f x i = x + (f i)
+
 sinPrecisions :: Double -> Stream Double
-sinPrecisions = todo
+sinPrecisions x =
+  let f = sinFun $ sinBoundary $ x in generateWithIndex 0 0 (series f)
 
 ePrecisions :: Stream Rational
-ePrecisions = todo
+ePrecisions = generateWithIndex 0 0 (series eFun)
